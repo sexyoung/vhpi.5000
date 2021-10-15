47bb241fc86972dc43bd982106a97f320275d8e7
sexyoung <Habuche@gmail.com>
2021-10-15 18:52:44 +0800

    feat: 請輸入後三碼查詢

--- src/App.tsx
+++ src/App.tsx
@@ -90,6 +90,7 @@ const App: React.FC = () => {
       <div className={style.text}>身份證加碼查</div>
       <small>非官方，加碼券得獎請看<a href="https://vhpi.5000.gov.tw/" target="_blank">振興五倍券官網</a></small>
       <form onSubmit={handleSubmit}>
+        <div>請輸入後三碼查詢</div>
         <input type="tel" id="code" maxLength={3} onChange={handleChange} />
         <button disabled={disabled}>查詢</button>
       </form>
