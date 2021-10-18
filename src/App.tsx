32e2e4b80d20d4a1e37d4f4bb16226577f3e94dd
LuckyPigeon <lucky90322@gmail.com>
2021-10-19 01:16:52 +0800

    新增券種面額

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,17 @@ const linkMapping = {
   rgionalRevitalization: 'https://www.twrr.ndc.gov.tw/',
 }
 
+const valueMapping = {
+  domesticTravel: '1000',
+  iYuan: '1000',
+  agriculture: '888',
+  artFunE: '600',
+  artFunP: '600',
+  sports: '500',
+  hakka: '500',
+  rgionalRevitalization: '500',
+}
+
 const images = {
   domesticTravel,
   iYuan,
@@ -112,7 +123,7 @@ const App: React.FC = () => {
               <div className={style.title}>Wooow 中獎了</div>
               {resultList.map((result, index) =>
                 <div key={index} className="result">
-                  第{result.split('-')[0]}期{nameMapping[result.split('-')[1] as keyof typeof nameMapping]}
+                  第{result.split('-')[0]}期{nameMapping[result.split('-')[1] as keyof typeof nameMapping]} ({valueMapping[result.split('-')[1] as keyof typeof valueMapping]}元)
                   <a href={linkMapping[result.split('-')[1] as keyof typeof linkMapping]} target="_blank" rel="noreferrer">
                     <img alt="prize" src={images[result.split('-')[1] as keyof typeof images]} />
                   </a>
